6b9e750232ab65b3cdc19e30feaf0a084f846177
Walter Prince <wprince0288@gmail.com>
2025-01-09 23:01:30 -0600

    ep1

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,11 +1,21 @@
 // implement your posts router here
 const express = require('express')
-// const Post = require('./posts/posts-model');
+const Post = require('./posts-model');
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-res.json('nerd')
+    Post.find()
+        .then(found => {
+           res.json(found)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The posts information could not be retrieved",
+                err: err.message,
+                stack: err.stack,
+            })
+        })
 })
 router.get('/:id', (req, res) => {
 
