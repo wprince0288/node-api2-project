11e09ebf37f1258731f674d0a1f8b0806071385c
Walter Prince <wprince0288@gmail.com>
2025-01-09 23:42:20 -0600

    ep4

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -61,8 +61,24 @@ router.post('/', (req, res) => {
     }
 })
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist",
+            })
+        } else {
+            await Post.remove(req.params.id)
+            res.json(post)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post could not be removed",
+            err: err.message,
+            stack: err.stack,
+        })
+    }
 })
 
 router.put('/:id', (req, res) => {
