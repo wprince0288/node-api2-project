c01027ae3fe0f44e6ea0fdb4f485dcb77fedd755
Walter Prince <wprince0288@gmail.com>
2025-01-09 23:17:47 -0600

    ep2

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -7,7 +7,7 @@ const router = express.Router()
 router.get('/', (req, res) => {
     Post.find()
         .then(found => {
-           res.json(found)
+            res.json(found)
         })
         .catch(err => {
             res.status(500).json({
@@ -17,8 +17,23 @@ router.get('/', (req, res) => {
             })
         })
 })
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist",
+            })
+        } else {
+            res.json(post)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post information could not be retrieved",
+            err: err.message,
+            stack: err.stack,
+        })
+    }
 })
 router.post('/', (req, res) => {
 
