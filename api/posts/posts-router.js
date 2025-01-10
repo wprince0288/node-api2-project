e2e2e768680bbd311fa9d8d7444f78f9d22a1220
Walter Prince <wprince0288@gmail.com>
2025-01-09 21:45:12 -0600

    basic server started

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,10 @@
 // implement your posts router here
+// const express = require('express')
+// const Post = require('./posts/posts-model');
+
+// const router = express.Router()
+
+// router.get('/api/posts', (req, res) => {
+//     Post.find(req.query)
+    
+// })
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it here
+const server = require('./api/server')
+
+const PORT = 8000
+
+server.listen(PORT, () => {
+    console.log(`\n*** Server Running on http://localhost${PORT} ***\n`)
+})
