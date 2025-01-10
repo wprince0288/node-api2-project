d5f94d7067a3b2933f39361d0c6686fc796e0b3d
Walter Prince <wprince0288@gmail.com>
2025-01-09 21:58:27 -0600

    posts-router started

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,10 +1,27 @@
 // implement your posts router here
-// const express = require('express')
+const express = require('express')
 // const Post = require('./posts/posts-model');
 
-// const router = express.Router()
+const router = express.Router()
 
-// router.get('/api/posts', (req, res) => {
-//     Post.find(req.query)
-    
-// })
+router.get('/', (req, res) => {
+res.json('nerd')
+})
+router.get('/:id', (req, res) => {
+
+})
+router.post('/', (req, res) => {
+
+})
+router.delete('/:id', (req, res) => {
+
+})
+router.put('/:id', (req, res) => {
+
+})
+router.get('/:id/messages', (req, res) => {
+
+})
+
+
+module.exports = router
