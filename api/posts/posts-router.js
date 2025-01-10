9ba0221fa2c521b5755f11450bfda69243f0f332
Walter Prince <wprince0288@gmail.com>
2025-01-09 23:49:41 -0600

    ep5

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -82,7 +82,40 @@ router.delete('/:id', async (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-
+    const { title, contents } = req.body
+    if (!title || !contents) {
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+        Post.findById(req.params.id)
+            .then(stuff => {
+                if (!stuff) {
+                    res.status(404).json({
+                        message: "The post with the specified ID does not exist",
+                    })
+                } else {
+                    return Post.update(req.params.id, req.body)
+                }
+            })
+            .then(data => {
+                if (data) {
+                    return Post.findById(req.params.id)
+                }
+            })
+            .then(post => {
+                if (post) {
+                    res.json(post)
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "The posts information could not be retrieved",
+                    err: err.message,
+                    stack: err.stack,
+                })
+            })
+    }
 })
 
 router.get('/:id/messages', (req, res) => {
